6e0475fdbb3e0403c68c590d111c58fa7f972563
Mahak Narayan <makk.bit@outlook.com>
2017-10-09 21:58:50 +0530

    do ll

---
+++ LinkedList/LinkedList.js
@@ -0,0 +1,77 @@
+
+class Node{
+  constructor(element){
+    this.element = element;
+    this.next = null;
+  }
+};
+
+class LinkedList{
+  constructor(){
+    this.length = 0;
+    this.head = null;
+  }
+  size() {
+    return this.length;
+  }
+  getHead() {
+    return this.head;
+  }
+  isEmpty(){
+    return this.length === 0;
+  }
+  indexOf(element){
+    let index = 0;
+    let current = this.head;
+    while(current){
+      index++;
+      if( current.element === element )
+        return index-1;
+      current = current.next;
+    }
+    return -1;
+  }
+  add(element){
+    const node = new Node(element);
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (true) {
+        if (current.next === null) {
+          current.next = node;
+          this.length++;
+          return;
+        }
+        current = current.next;
+      }
+    }
+  }
+  remove(element) {
+    if (this.head === null) {
+      return null;
+    }
+    let currentNode = this.head;
+    let previousNode = null;
+    if (currentNode.element === element) {
+      head = currentNode.next;
+    } else {
+      while (currentNode.element !== element) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      previousNode.next = currentNode.next;
+    }
+  }
+}
+
+
+const ll = new LinkedList();
+ll.add('mahak');
+ll.add('jeff');
+ll.add('matt');
+ll.add('jason');
+ll.add('fin');
+ll.remove('matt');
+console.log(ll.getHead());
+console.log(ll.indexOf('matt'));
