a58ee93252ed49f043b67d73f4686b6ca85ab180
Mahak Narayan <makk.bit@outlook.com>
2017-12-05 17:07:10 +0530

    add Trie

---
+++ Trie/Trie.py
@@ -0,0 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None]*26
+        self.isEndOfWord = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = self.getNode()
+    def getNode(self):
+        return TrieNode()
+    def _charToIndex(self, ch):
+        return ord(ch)-ord('a')
+    def insert(self, key):
+        pCrawl = self.root
+        for level in range(len(key)):
+            index = self._charToIndex( key[level] )
+            if not pCrawl.children[index]:
+                pCrawl.children[index] = self.getNode()
+            pCrawl = pCrawl.children[index]
+        pCrawl.isEndOfWord = True
+    def search(self, key):
+        pCrawl = self.root
+        for level in range(len(key)):
+            index = self._charToIndex( key[level] )
+            if not pCrawl.children[index]:
+                return False
+            pCrawl = pCrawl.children[index]
+        return pCrawl != None and pCrawl.isEndOfWord
+
+t = Trie()
+
+keys = ['the', 'there', 'a', 'answer', 'any', 'by', 'their']
+
+for k in keys:
+    t.insert(k)
+
+print t.search('the')
+print t.search('there')
+print t.search('a')
+print t.search('answer')
+print t.search('any')
+print t.search('by')
+print t.search('their')
+print t.search('wtf')
